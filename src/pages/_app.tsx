a3b0b07a63f6551405e6f543993b4c4286d6c5c1
codyNog <yoneyoneyone99@gmail.com>
2021-08-05 00:45:23 +0900

    不要なcomponentを削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,16 +7,6 @@ if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
 }
 
 export default class MyApp extends App {
-  static async getStaticProps({ Component, ctx }) {
-    let pageProps = {};
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getStaticProps(ctx);
-    }
-
-    return { pageProps };
-  }
-
   render() {
     const { Component, pageProps } = this.props;
 
